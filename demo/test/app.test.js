f9fd69f8ad3985edc30776dfd0adec5ca8010ce0
Canh Huynh <158004919+canhhuynh220805@users.noreply.github.com>
2026-04-02 13:32:52 +0700

    Delete demo directory

--- demo/test/app.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const test = require("node:test");
-const assert = require("node:assert");
-const {add} = require("../src/index");
-
-test("add", () => {
-  assert.strictEqual(add(1, 2), 3);
-});
